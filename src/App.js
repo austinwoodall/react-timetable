31b0ec442aea70e7e4ae576f50db248de9acedd9
Austin <austin.woodall72@gmail.com>
2020-09-08 18:15:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dayjs from "dayjs";
+const weekday = require('dayjs/plugin/weekday');
+const localeData = require('dayjs/plugin/localeData')
+dayjs.extend(localeData)
+dayjs.extend(weekday)
+dayjs().localeData()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const firstDayOfWeek = dayjs().weekday(0);
+    const lastDayOfWeek = dayjs().weekday(6);
+    const daysOfWeek = dayjs.weekdays();
+
+    useEffect(() => {
+        console.log(firstDayOfWeek);
+        console.log(dayjs().hour())
+        console.log(dayjs().minute())
+    }, [])
+    return (
+        <div style={{ display: "flex", flexDirection: "row", width: "100%", overflow: "scroll" }} className="App">
+            <div className={"scrolling-container"} style={{ display: "flex", width: "100%", whiteSpace: "nowrap", flexDirection: "row", justifyContent: "space-evenly"}}>
+                {
+                    daysOfWeek.map((day, index) => {
+                        const weekday = dayjs().weekday(index)
+                        return (
+                            <div key={index} style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                padding: "0 2px"
+                            }}>
+                                <h3 style={{ margin: 0 }}>{day}</h3>
+                                <p style={{ margin: 0, fontSize: 12 }}>{weekday.format("MM.DD.YYYY")}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
